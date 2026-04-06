736830cb089ab5e1e279766d920da585fed8ab5c
Lucas Bugiolachi <lbugiola@gmail.com>
2026-04-06 09:28:35 -0300

    --fix Pulse dashboard and permissions

--- crm-si-back/app/Providers/AppServiceProvider.php
+++ crm-si-back/app/Providers/AppServiceProvider.php
@@ -3,7 +3,10 @@
 namespace App\Providers;
 
 use App\Models\Message;
+use App\Enums\UserRole;
+use App\Models\User;
 use App\Observers\MessageObserver;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,5 +25,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Message::observe(MessageObserver::class);
+
+        Gate::define('viewPulse', function (User $user) {
+            return $user->role === UserRole::ADMIN;
+        });
     }
 }
